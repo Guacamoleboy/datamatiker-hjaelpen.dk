9c4b13c7d8ee4532e975137855070f0cc383ed05
Guacamoleboy <twitch@galleripermoller.dk>
2025-04-13 22:41:19 +0200

    Darkmode, nav update, css update and much more!

---
+++ Website/assets/js/darkmode.js
@@ -0,0 +1,29 @@
+const toggleBtn = document.getElementById('darkModeToggle');
+    const body = document.body;
+    const logo = document.getElementById('logo');
+
+    const setDarkMode = () => {
+        body.classList.add('dark-mode');
+        toggleBtn.textContent = 'Lightmode ☀️';
+        logo.src = 'assets/images/logo/logo2-white.png';
+        localStorage.setItem('mode', 'dark');
+    };
+
+    const setLightMode = () => {
+        body.classList.remove('dark-mode');
+        toggleBtn.textContent = 'Darkmode 🌙';
+        logo.src = 'assets/images/logo/logo2-resized.png';
+        localStorage.setItem('mode', 'light');
+    };
+
+    if (localStorage.getItem('mode') === 'dark') {
+        setDarkMode();
+    }
+
+    toggleBtn.addEventListener('click', () => {
+        if (body.classList.contains('dark-mode')) {
+            setLightMode();
+        } else {
+            setDarkMode();
+        }
+    });
