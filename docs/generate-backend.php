10401b59bbe7a9bc78e75f8a413cf27b46c296a0
Guacamoleboy <twitch@galleripermoller.dk>
2025-04-21 12:17:33 +0200

    yeayaeye

--- docs/generate-backend.php
+++ docs/generate-backend.php
@@ -15,9 +15,27 @@ $fullHTML = <<<HTML
 <!DOCTYPE html>
 <html lang="da">
 <head>
+    <!-- Required Meta Tags -->
     <meta charset="UTF-8">
-    <title>Custom Side</title>
-    <link rel="stylesheet" href="/assets/styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Page Title -->
+    <title>Datamatiker-hjaelpen.dk</title> 
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="../assets/images/image1.png" type="image/x-icon">
+
+    <!-- Navbar load -->
+    <script src="../assets/js/navbar2.js" defer></script>
+
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="../assets/css/animate-3.7.0.css">
+    <link rel="stylesheet" href="../assets/css/font-awesome-4.7.0.min.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap-4.1.3.min.css">
+    <link rel="stylesheet" href="../assets/css/owl-carousel.min.css">
+    <link rel="stylesheet" href="../assets/css/jquery.datetimepicker.min.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
     <div class="deshes-area section-padding text-center mt-5">
