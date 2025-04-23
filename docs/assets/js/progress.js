4e21d6ee9af5027fe884f836c341c024beaf3920
Guacamoleboy <twitch@galleripermoller.dk>
2025-04-23 13:49:28 +0200

    updates

--- docs/assets/js/progress.js
+++ docs/assets/js/progress.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // __________________ Objekter Status ______________________
 
-    const total01 = 3;
+    const total01 = "?";
     let correct01 = 0;
 
     for (let i = 1; i <= total01; i++) {
@@ -52,7 +52,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // __________________ ArrayList Status ______________________
 
-    const total02 = 3;
+    const total02 = 4;
     let correct02 = 0;
 
     for (let i = 1; i <= total02; i++) {
